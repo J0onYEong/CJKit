5d9a5316474a6d263e4c49ee1b09155149d3f24d
J0onYEong <cjy2134@naver.com>
2024-02-06 14:59:57 +0900

    [Feat] DynamicText 구현
    정적인 영역에 대해서만 동작한다.

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "CJKit",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CJKit",
-            targets: ["CJKit"]),
+            targets: ["CustomUI"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CJKit"),
+            name: "CustomUI"),
         .testTarget(
             name: "CJKitTests",
-            dependencies: ["CJKit"]),
+            dependencies: ["CustomUI"]),
     ]
 )
---
+++ Sources/CustomUI/DynamicText.swift
@@ -0,0 +1,169 @@
+//
+//  DynamicText.swift
+//
+//
+//  Created by 최준영 on 2/5/24.
+//
+
+import SwiftUI
+import UIKit
+
+struct DynamicText: View {
+    
+    var content: String
+    
+    var weight: UIFont.Weight
+    
+    var lineCount: Int
+    
+    var kern: CGFloat
+    
+    var linespacing: CGFloat
+    
+    var textAlignment: NSTextAlignment
+    
+    init(_ content: String, weight: UIFont.Weight = .regular, lineCount: Int = 1, kern: CGFloat = 0, linespacing: CGFloat = 5, textAlignment: NSTextAlignment = .left) {
+        self.content = content
+        self.weight = weight
+        self.lineCount = lineCount
+        self.kern = kern
+        self.linespacing = linespacing
+        self.textAlignment = textAlignment
+    }
+    
+    var body: some View {
+        
+        GeometryReader { geo in
+            
+            UILabelRepresentable(content, weight: weight, viewSize: geo.size, lineCount: lineCount, kern: kern, linespacing: linespacing, textAlignment: textAlignment)
+        }
+        
+    }
+    
+}
+
+private extension DynamicText {
+    
+    class StringCoordinator {
+        
+        // Attrs
+        var stringAttributes: [NSMutableAttributedString.Key : Any] = [:]
+        
+    }
+    
+    struct UILabelRepresentable: UIViewRepresentable {
+        
+        var content: String
+        
+        var viewSize: CGSize
+        
+        var lineCount: Int
+        
+        var font: UIFont
+        
+        var kern: CGFloat
+        
+        var linespacing: CGFloat
+        
+        var textAlignment: NSTextAlignment
+        
+        private let textView: UITextView = {
+            let textView = UITextView()
+            
+            textView.textContainer.lineBreakMode = .byCharWrapping
+            textView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            textView.textContainer.lineFragmentPadding = 0
+            
+            return textView
+        }()
+        
+        init(_ content: String, weight: UIFont.Weight, viewSize: CGSize, lineCount: Int, kern: CGFloat = 0.0, linespacing: CGFloat = 5.0, textAlignment: NSTextAlignment = .left) {
+            self.content = content
+            self.viewSize = viewSize
+            self.lineCount = lineCount
+            self.font = .systemFont(ofSize: 0.1, weight: weight)
+            self.kern = kern
+            self.linespacing = linespacing
+            self.textAlignment = textAlignment
+        }
+        
+        func makeUIView(context: Context) -> UITextView {
+            
+            let coordinator = context.coordinator
+            
+            let style = NSMutableParagraphStyle()
+            
+            style.lineSpacing = linespacing
+            style.alignment = textAlignment
+            
+            coordinator.stringAttributes = [
+                .font : self.font,
+                .kern : self.kern,
+                .paragraphStyle : style
+            ]
+            
+            textView.bounds.size = viewSize
+            
+            return textView
+        }
+        
+        func updateUIView(_ uiView: UITextView, context: Context) {
+            
+            let coordinator = context.coordinator
+            var prevFont: UIFont = font
+            var newFont: UIFont = font
+            
+            while(true) {
+                newFont = prevFont.withSize(prevFont.pointSize+0.1)
+                
+                coordinator.stringAttributes[.font] = newFont
+                
+                var errorInset: CGSize = .zero
+                
+                if lineCount >= 2 {
+                    errorInset = "한".size(withAttributes: coordinator.stringAttributes)
+                }
+
+                let textSize = content.size(withAttributes: coordinator.stringAttributes)
+                
+                // 가로영역이 더 커지거나 세로영역이 더커지는 경우 그전 조정값이 가장 적절한 조정값이다.
+                if textSize.width > viewSize.width*CGFloat(lineCount)-errorInset.width || textSize.height > viewSize.height/CGFloat(lineCount) - linespacing*CGFloat(lineCount-1) {
+                    
+                    coordinator.stringAttributes[.font] = prevFont
+                    
+                    // 수직적 정렬을 위해 높이를 파악하여 패딩을 추가한다.
+                    let textHeight = content.size(withAttributes: coordinator.stringAttributes).height * CGFloat(lineCount) + linespacing * CGFloat(lineCount-1)
+                    let verticalPadding = (viewSize.height - textHeight)/2
+                    
+                    uiView.textContainerInset.top = verticalPadding
+                    
+                    let attributedString = NSMutableAttributedString(string: content)
+                    
+                    attributedString.addAttributes(coordinator.stringAttributes, range: NSRange(location: 0, length: attributedString.length))
+                    
+                    uiView.attributedText = attributedString
+                    
+                    break
+                }
+                
+                prevFont = newFont
+            }
+        }
+        
+        func makeCoordinator() -> StringCoordinator {
+            StringCoordinator()
+        }
+        
+        typealias UIViewType = UITextView
+    }
+}
+
+
+
+#Preview {
+    
+    DynamicText("안녕하세요 반갑습니다 다음에 또만날수 있다면 정말 좋을 것 같아요!!!!", lineCount: 1)
+        .frame(width: 300, height: 64)
+        .border(.red)
+    
+}
